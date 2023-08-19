d8095789aba9101a03ec25f3ed717b70870e2b76
Tethet <ayush.kumar.cse22@itbhu.ac.in>
2023-08-19 18:53:21 +0530

    Making Changes to the files and adding new components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import "./components/navbar/navbar.js"
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={Login}/>
+          <Route path='/login' element={Login}/>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
