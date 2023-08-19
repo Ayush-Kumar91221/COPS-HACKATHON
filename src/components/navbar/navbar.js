9dd90894d5715753108399a1badaf21b5442c1a5
Tethet <ayush.kumar.cse22@itbhu.ac.in>
2023-08-19 18:28:06 +0530

    first commit

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,11 @@
+import "./navbar.css"
+const Navbar =()=>{
+    <nav class="navbar">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/help">FAQs</a>
+        <a href="">Portfolio</a>
+        <a href="/contact">Contact Us</a>
+    </nav>
+}
+export default Navbar;
